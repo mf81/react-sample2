c55ae6aa30108f71c9b7b86ab73ad256a4fc2ec7
mf81 <35445517+mf81@users.noreply.github.com>
2022-03-01 18:45:51 +0100

    Setup project

---
+++ src/components/fakeDate.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+class FakeDate extends Component {
+  state = {};
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <h1>React Sample 2</h1>
+          <h2>Fake Data Base</h2>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default FakeDate;
